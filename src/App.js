dce5caf4c46af795710e5fbc04d82c1eec9eebf2
tionna <tionnawilsonpierce@gmail.com>
2016-10-03 16:48:28 -0400

    updated files to match master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Apperal from './apperal';
-import data from '../data.js';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
-
+//components 
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import FoundError from './FoundError'
+import HomePage from './HomePage'
+import CategoryPage from './CategoryPage'
+import ProductPage from './ProductPage'
+import data from './data'
 
 var App = React.createClass({
-  render: function() {
+
+  getInitialState(){
+    return {data: data, listOfItems: '', filteredList: ''}
+  },
+
+  componentDidMount(){
+
+    //create a list of all data items
+    var listOfItems = []
+    Object.keys(data).map(function(category){
+      return data[category].map(function(item){
+          listOfItems.push(item.name.toLowerCase())
+      })
+    })
+    this.setState({listOfItems: listOfItems, filteredList: listOfItems})
+  },
+
+  handleItemSearch(item){
+    function sameLetters(filteredListItem) {
+      if(filteredListItem.indexOf(item) !== -1){
+        return filteredListItem
+      }
+    }
+    var searchItems = this.state.filteredList.filter(
+    sameLetters)
+    
+    this.setState({filteredList: searchItems})
+  },
+  handleSearchReset: function() {
+    this.setState({filteredList: this.state.listOfItems})
+  },
+  render() {
+    console.log(this.state.filteredList)
     return (
       <div>
-        <h1 id="items">Products</h1>
+        <Nav onChange={this.handleItemSearch} onReset={this.handleSearchReset}/>
         {this.props.children}
+        <Footer />
       </div>
     )
   }
-});
+})
+
 
 ReactDOM.render(
-<App/>,
-document.getElementById('root')
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={HomePage} />
+      <Route path=":category" component={CategoryPage} />
+      <Route path=":category/:product" component={ProductPage} />
+      <Route path="*" component={FoundError} />
+    </Route> 
+  </Router>,
+  document.getElementById('root')
 );
-
-
